4cc6fb7a32a7e100c09e93a503bd0c9ff565d69f
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-02-23 16:15:21 +0100

    basa radi

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import Link from 'next/link'
+ 
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Amplify from 'aws-amplify'
+import config from '../src/aws-exports'
+Amplify.configure({
+  ...config,
+  ssr: true
+})
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav  >
+        <Link href="/">
+          <span  >Home</span>
+        </Link>
+        <Link href="/profile">
+          <span  >Profile</span>
+        </Link>
+        <Link href="/protected">
+  <span  >Protected route</span>
+</Link><Link href="/protected-client-route">
+ <span  >Protected client route</span>
+</Link>
+
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
-
-export default MyApp
