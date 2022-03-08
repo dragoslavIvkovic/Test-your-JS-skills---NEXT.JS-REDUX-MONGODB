c6e454e5594b3f01bdaccf0a2ec960ce50e36f67
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-03-08 20:30:09 +0100

    added QApage to mongodb

--- pages/QApage.js
+++ pages/QApage.js
@@ -1,23 +1,75 @@
+import React, { useState } from "react";
+ 
 import { connectToDatabase } from "../util/mongodb";
 
 export default function Questions({ questions }) {
-  return (
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+
+
+  function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
+
+
+
+ return (
     <div>
-      <h1>Top 20 Movies of All Time</h1>
-      <p>
-        <small>(According to Metacritic)</small>
-      </p>
-      <ul>
-        {questions.map((x) => (
-          <li key={x.id}>
-            <h2>{x.questionText}</h2>
-           
-          </li>
-        ))}
-      </ul>
+      <div></div>
+      <div className="app">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button key={uuidv4()}
+                  onClick={() =>
+                    handleAnswerOptionClick(answerOption.isCorrect)
+                  }
+                >
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
+
 
 export async function getStaticProps() {
   const { db } = await connectToDatabase();
