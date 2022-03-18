2a457de077d948a13715b995ca0a68a22351d88f
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-03-18 22:16:10 +0100

    modal + reset state on begining

--- pages/QApage.js
+++ pages/QApage.js
@@ -2,28 +2,39 @@ import React, { useState } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 import {
-  incrementByAmount,
+   
   increment,
   reset
 } from '../store/reducers/counterSlice'
 import { connectToDatabase } from '../util/mongodb'
 import { CopyBlock, dracula } from 'react-code-blocks'
 import styles from '../styles/Qpage.module.css'
-import { shuffle } from '../util/shuffle'
+ 
 import shuffleArray from '../util/shuffle'
 import uuidv from '../util/uuidv'
+import { Modal, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 
 export default function Questions ({ questions }) {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [showScore, setShowScore] = useState(false)
 
+  const [open, setOpen] =  useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+     dispatch(reset())
+   setOpen(false);
+  }
+
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
   const score = Object.values(counter)
-
+console.log(counter)
   const handleAnswerOptionClick = isCorrect => {
     if (isCorrect) {
       dispatch(increment())
+    } else {
+
     }
 
     const nextQuestion = currentQuestion + 1
@@ -36,9 +47,44 @@ export default function Questions ({ questions }) {
 
   const shuffle = () => 0.5 - Math.random()
 
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1000,
+  height: 800,
+  bgcolor: 'red',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
   return (
     <div className={styles.container}>
-      <div></div>
+      <div>
+ <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+           Click outside to start test
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            
+          </Typography>
+        </Box>
+      </Modal>
+
+
+      </div>
       <div className='app'>
         {showScore ? (
           <div className='score-section'>
