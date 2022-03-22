edc6116879860015ebce4b13efd3a7e1b8784461
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-03-22 14:41:09 +0100

    stage

--- pages/QApage.js
+++ pages/QApage.js
@@ -37,9 +37,7 @@ export default function Questions () {
 
   const fetchQuestions = async () => {
     const response = await fetch('/api/QApages')
-
     const data = await response.json()
-
     setQuestions(data)
   }
 
@@ -79,74 +77,75 @@ export default function Questions () {
   }
 
   return (
-    <div>
-      <button onClick={fetchQuestions}> dfssdf </button>
-      <div className={styles.container}>
-        <div>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby='modal-modal-title'
-            aria-describedby='modal-modal-description'
-          >
-            <Box sx={style}>
-              <Typography id='modal-modal-title' variant='h6' component='h2'>
-                Click outside to start test
-              </Typography>
-              <Typography
-                id='modal-modal-description'
-                sx={{ mt: 2 }}
-              ></Typography>
-            </Box>
-          </Modal>
-        </div>
+    <button onClick={fetchQuestions}>fdsf</button>
+    // <div>
+    //   <button onClick={fetchQuestions}> dfssdf </button>
+    //   <div className={styles.container}>
+    //     <div>
+    //       <Modal
+    //         open={open}
+    //         onClose={handleClose}
+    //         aria-labelledby='modal-modal-title'
+    //         aria-describedby='modal-modal-description'
+    //       >
+    //         <Box sx={style}>
+    //           <Typography id='modal-modal-title' variant='h6' component='h2'>
+    //             Click outside to start test
+    //           </Typography>
+    //           <Typography
+    //             id='modal-modal-description'
+    //             sx={{ mt: 2 }}
+    //           ></Typography>
+    //         </Box>
+    //       </Modal>
+    //     </div>
 
        
-        <div className='app'>
-          {showScore ? (
-            <div className='score-section'>
-              You scored {score} out of {questions.length}
-            </div>
-          ) : (
-            <>
-              <div>
-                <div className='question-count'>
-                  <span>Question {currentQuestion + 1}</span>/{questions.length}
-                </div>
-                <div className='question-text'>
-                  {questions[currentQuestion].questionText}
-                </div>
-              </div>
-              <CopyBlock
-                language='javascript'
-                text={JSON.stringify(questions[currentQuestion].code).replace(
-                  /(^"|"$)/g,
-                  ''
-                )}
-                theme={dracula}
-                wrapLines={true}
-                highlight='1'
-                codeBlock
-              /> 
-              <div className={styles.answer_section}>
-                {questions[currentQuestion].answerOptions
-                  .sort(shuffle)
-                  .map(answerOption => (
-                    <button
-                      className={styles.answer}
-                      key={questions._id}
-                      onClick={() =>
-                        handleAnswerOptionClick(answerOption.isCorrect)
-                      }
-                    >
-                      {answerOption.answerText}
-                    </button>
-                  ))}
-              </div>
-            </>
-          )}
-        </div> 
-      </div>
-    </div>
+    //     <div className='app'>
+    //       {showScore ? (
+    //         <div className='score-section'>
+    //           You scored {score} out of {questions.length}
+    //         </div>
+    //       ) : (
+    //         <>
+    //           <div>
+    //             <div className='question-count'>
+    //               <span>Question {currentQuestion + 1}</span>/{questions.length}
+    //             </div>
+    //             <div className='question-text'>
+    //               {questions[currentQuestion].questionText}
+    //             </div>
+    //           </div>
+    //           <CopyBlock
+    //             language='javascript'
+    //             text={JSON.stringify(questions[currentQuestion].code).replace(
+    //               /(^"|"$)/g,
+    //               ''
+    //             )}
+    //             theme={dracula}
+    //             wrapLines={true}
+    //             highlight='1'
+    //             codeBlock
+    //           /> 
+    //           <div className={styles.answer_section}>
+    //             {questions[currentQuestion].answerOptions
+    //               .sort(shuffle)
+    //               .map(answerOption => (
+    //                 <button
+    //                   className={styles.answer}
+    //                   key={questions._id}
+    //                   onClick={() =>
+    //                     handleAnswerOptionClick(answerOption.isCorrect)
+    //                   }
+    //                 >
+    //                   {answerOption.answerText}
+    //                 </button>
+    //               ))}
+    //           </div>
+    //         </>
+    //       )}
+    //     </div> 
+    //   </div>
+    // </div>
   )
 }
