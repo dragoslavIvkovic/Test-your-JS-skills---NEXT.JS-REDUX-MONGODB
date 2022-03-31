5128f1bdf3a91bbff695d9aa94e5847fec3403a8
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-03-31 15:51:15 +0200

    conditional rendering fixed

--- pages/QApage.js
+++ pages/QApage.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increment, reset } from '../store/reducers/counterSlice'
 
 // import { CopyBlock, dracula } from 'react-code-blocks';
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';;
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 import styles from '../styles/Qpage.module.css'
 
@@ -20,7 +20,7 @@ export default function Questions () {
 
   const [questions, setQuestions] = useState({})
   const [collection, setCollection] = useState()
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   const [open, setOpen] = useState(true)
   const handleOpen = () => setOpen(true)
@@ -42,12 +42,12 @@ export default function Questions () {
     const response = await fetch(`/api/QApages?collection=${collection}`)
     const data = await response.json()
     setQuestions(shuffleArray(data))
-    setLoading(false)
+    setLoading(true)
   }
 
   console.log('questions', questions)
   console.log('loading', loading)
-  console.log('collection', collection)
+  console.log('collection', collection === undefined)
 
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
@@ -82,44 +82,20 @@ export default function Questions () {
     p: 4
   }
 
-
-  console.log(loading)
-
   return (
     <>
-      <button onClick={fetchQuestions}>START</button>
-      <button onClick={() => setCollection('questions')}>questions</button>
-      <button onClick={() => setCollection('middle')}>middle</button>
-      <button onClick={() => setCollection('XXX')}>xxx</button>
       <div>
-        {' '}
-        {loading ? (
-          <div>Loading...</div>
+        {collection === undefined ? (
+          <div>
+            {' '}
+            <button onClick={() => setCollection('questions')}>
+              questions
+            </button>
+            <button onClick={() => setCollection('middle')}>middle</button>
+            <button onClick={() => setCollection('XXX')}>xxx</button>
+          </div>
         ) : (
-          <div className={styles.container}>
-            <div>
-              {/* <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby='modal-modal-title'
-                aria-describedby='modal-modal-description'
-              >
-                <Box sx={style}>
-                  <Typography
-                    id='modal-modal-title'
-                    variant='h6'
-                    component='h2'
-                  >
-                    Click outside to start test
-                  </Typography>
-                  <Typography
-                    id='modal-modal-description'
-                    sx={{ mt: 2 }}
-                  ></Typography>
-                </Box>
-              </Modal> */}
-            </div>
-
+          <div> {loading ? (
             <div className='app'>
               {showScore ? (
                 <div className='score-section'>
@@ -160,8 +136,16 @@ export default function Questions () {
                 </>
               )}
             </div>
+          ) :
+          
+          
+          
+            <button onClick={fetchQuestions}>START</button>
+          }
           </div>
         )}
+
+        <div></div>
       </div>
     </>
   )
