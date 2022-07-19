c70ee56db6e343f70b926e2580a6fbdcc431cc58
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2022-07-19 13:01:34 +0200

    loading implement

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -7,10 +9,27 @@ import Head from 'next/head';
 import store from '../store/store';
 import Header from '../components/Header';
 import 'normalize.css';
+import Loading from "../components/Loading";
 
 const persistor = persistStore(store);
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
+
+useEffect(() => {
+    const handleStart = (url) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = (url) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
+
   return (
     
       <Provider store={store}>
@@ -21,8 +40,10 @@ export default function MyApp({ Component, pageProps }) {
               name="viewport"
               content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
             />
-          </Head>
-          <Component {...pageProps} />
+        </Head>
+        {loading ?
+          <Loading loading={loading} /> :
+          <Component {...pageProps} />}
         </PersistGate>
         
       </Provider>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 import styles from "../styles/Elements.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main />
-      <footer />
+      <Link href="/Test">
+            <a className={styles.link}>SKIP TUTORIAL</a>
+          </Link>
     </div>
   );
 }
